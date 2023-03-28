05b1336cbed8399509145e9a9ae2e903ed8136b7
kritisharma1202 <kri.sha1202@gmail.com>
2023-03-28 16:14:15 -0500

    add delete user functionality, implement to make user as admin user and allow admin to perform additional authorities

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -57,7 +57,7 @@ class ArticlesController < ApplicationController
   end
 
   def require_same_user
-    if current_user != @article.user
+    if current_user != @article.user && !current_user.admin?
       flash[:alert] = "You can only Edit or Delete your own article"
       redirect_to @article
     end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update]
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
   before_action :require_user, only: [:edit, :update]
-  before_action :require_same_user, only: [:edit, :update]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
   
   def show
     #@articles = @user.articles
@@ -40,6 +40,13 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    @user.destroy #to delete user account
+    session[:user_id] = nil if @user == current_user
+    flash[:alert] = "Your account and all associated articles to your account is successfully deleted"
+    redirect_to root_path
+  end
+
   private
   def user_params
     params.require(:user).permit(:username, :email, :password)
@@ -50,8 +57,8 @@ class UsersController < ApplicationController
   end
 
   def require_same_user
-    if current_user != @user
-      flash[:alert] = "You can only Edit your own account"
+    if current_user != @user && !current_user.admin?
+      flash[:alert] = "You can only Edit and Delete your own account"
       redirect_to @user
     end
   end
